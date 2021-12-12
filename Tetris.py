8bf9d37f4975ebbf977df7cee22c385e9ad2d113
ToyodaKeisuke <toyodakeisuke4000@gmail.com>
2021-12-12 19:56:13 +0900

    タイトルロゴが表示されるようにした。また、インスタンスをGameRootクラス外で作成することでどこでもクラス情報が共有されるようにした。

--- Tetris.py
+++ Tetris.py
@@ -15,48 +15,64 @@ class GameRoot(object):#テトリスの根幹
     WINDOW_SIZE = (800,600)
     #ゲームの状態
     is_game_launched = False
-    is_game_closed = False
-    #背景のパス
-    ouspenski_cathedral_path = r'Images\OuspenskiCathedral\OuspenskiCathedral(400x300)PxcelArt.png'
+    is_game_start_screen = False
+    #画像のパス
+    #オムスク大聖堂
+    OUSPENSKI_CATHEDRAL_PATH = r'Images\OuspenskiCathedral\OuspenskiCathedral(400x300)PxcelArt.png'
+    #Tetrisロゴ(タイトル画面の)
+    TETRIS_TITLE_LOGO_PATH = r'Images\Title\Tetris.png'
     def start_game(self,):
         GameRoot.is_game_launched = True
         pygame.init()
         #
-        DrawGame()
-        GameRoutine()
+        #ウィンドウを作成
+        draw_game.create_window()
+        #スタート画面を描画
+        draw_game.draw_start_screen()
+        #ゲームルーティーンを開始
+        game_routine.start_game_routine()
     def close_game(self,):
         #ここでデータ保存?
         pygame.quit()#初期化を解除
-        GameRoot.is_game_closed = False
+        #ゲームの状態
+        GameRoot.is_game_launched = False
+        GameRoot.is_game_start_screen = False
         sys.exit()#終了
     
 class DrawGame(GameRoot):#描画クラス
     def __init__(self,):
-        #背景を読み込み
-        #オムスク大聖堂
-        self.ouspenski_cathedral = pygame.image.load(GameRoot.ouspenski_cathedral_path)
-        #背景を400x300から800x600にする
-        self.ouspenski_cathedral = pygame.transform.scale(self.ouspenski_cathedral,(800,600))
+        #画像を読み込み
+        #オムスク大聖堂(背景)
+        self.ouspenski_cathedral = pygame.image.load(GameRoot.OUSPENSKI_CATHEDRAL_PATH)
+        #Tetrisロゴ
+        self.tetris_title_logo = pygame.image.load(GameRoot.TETRIS_TITLE_LOGO_PATH)
+        #
+        #読み込んだ画像を適切な大きさにする
+        #背景(オムスク大聖堂)を400x300から800x600にする(WINDOW_SIZEにする)
+        self.ouspenski_cathedral = pygame.transform.scale(self.ouspenski_cathedral,GameRoot.WINDOW_SIZE)
+        self.tetris_title_logo = pygame.transform.scale(self.tetris_title_logo,(650,150))
         #####
-        #ウィンドウを作成
-        self.create_window()
-        #スタート画面を描画
-        self.draw_start_screen()
     def create_window(self,):#windowを作成する。
         self.window = pygame.display.set_mode(GameRoot.WINDOW_SIZE)#作成
         pygame.display.set_caption('Tetris')#windowのタイトル
-    def draw_start_screen(self,):
+    def draw_start_screen(self,):#タイトル画面の作成
+        GameRoot.is_game_start_screen = True
         #オムスクの大聖堂を背景にする
         self.window.blit(self.ouspenski_cathedral,(0,0))
-        
+        #タイトル追加(TETRIS)
+        self.window.blit(self.tetris_title_logo,(75,0))
 class GameRoutine(GameRoot):
     def __init__(self,):
+        pass
+    def start_game_routine(self,):
         while True:
             pygame.display.update()#画面をアップデート
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     game_root.close_game()
-
+    def game_mode_selection(self,):
+        pass
+                    
 class TetriminoOperation(GameRoot):#テトリミノの操作
     def __init__(self,):
         pass
@@ -79,4 +95,8 @@ class TetriminoOperation(GameRoot):#テトリミノの操作
 
 if __name__ == '__main__':
     game_root = GameRoot()
+    #インスタンスをあらかじめ作成
+    draw_game = DrawGame()#画像を読み込む
+    game_routine = GameRoutine()
+    #
     game_root.start_game()
